1827781da853cf8b857957b92fcc864a400eaebe
Venla Jokikokko <t9jove00@students.oamk.fi>
2020-04-08 18:24:46 +0300

    small changes

--- application/views/users/login.php
+++ application/views/users/login.php
@@ -1,13 +1,13 @@
 <?php echo form_open('Users/login'); ?>
     <div class="row">
       <div class="col-md-4 col-md-offset-4">
-        <h1 class="text-center"><?php echo $title; ?></h1>
-        <div class=form-group>
+        <h1 class="text-muted"><?php echo $title; ?></h1>
+        <div class="form-group">
             <input type="email" name="email" class="form-group" placeholder="Enter Email" required autofocus>
         </div>
       </div>
     </div>
-        <div class=form-group>
+        <div class="form-group">
         <input type="password" name="password" class="form-group" placeholder="Enter Password" required autofocus>
             </div>
         <button type="submit" class="btn btn-primary">Login</button>
