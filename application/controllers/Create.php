7b6c688bd1a5edc419677858a267dfed2140eb56
Venla Jokikokko <t9jove00@students.oamk.fi>
2020-04-21 17:29:27 +0300

    fixes

---
+++ application/controllers/Create.php
@@ -0,0 +1,74 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Create extends CI_Controller{
+
+  public function __construct()
+  {
+    parent::__construct();
+    $this->load->model('Create_model');
+  }
+
+  public function show_items(){
+    $data['items']=$this->Create_model->getItems();
+    //print_r($data);
+    $this->load->view('layouts/header');
+    $this->load->view('layouts/body');
+    $this->load->view('create', $data);
+    $this->load->view('layouts/footer');
+  }
+
+  public function insert_item(){
+    //print_r($this->input->post());
+    $insert_data=array(
+      'title'=>$this->input->post('title'),
+      'description'=>$this->input->post('description'),
+      'price'=>$this->input->post('price'),
+      'image'=>$this->input->post('image')
+    );
+    $this->db->set('created', 'NOW()', FALSE);
+    $this->db->insert('products', $data);
+    $test=$this->Create_model->addItem($insert_data);
+    //echo 'inserted '.$test. 'items';
+    redirect('create/show_items');
+  }
+
+  public function edit_item(){
+    $id=$this->input->post('idProducts');
+    $update_data=array(
+      'title'=>$this->input->post('title'),
+      'description'=>$this->input->post('description'),
+      'price'=>$this->input->post('price')
+    );
+    $test=$this->Create_model->updateItem($id, $update_data);
+    if($test==0){
+      $data['message']='You can not update this item';
+      $data['return_url']='show_items';
+      $data['page']='feedback/message_box';
+      $this->load->view('layouts/content',$data);
+    }
+    else{
+      $data['message']='Item updated succesfully';
+      $data['return_url']='show_items';
+      $data['page']='feedback/message_box';
+      $this->load->view('layouts/content',$data);
+    }
+  }
+
+  public function delete_item(){
+    $id=$this->input->post('idProducts');
+    $test=$this->Create_model->deleteItem($id);
+    if($test==0){
+      $data['message']='You can not delete this product';
+      $data['return_url']='show_items';
+      $data['page']='feedback/message_box';
+      $this->load->view('layouts/content',$data);
+    }
+    else{
+      $data['message']='Product deleted succesfully';
+      $data['return_url']='show_items';
+      $data['page']='feedback/message_box';
+      $this->load->view('layouts/content',$data);
+      }
+    }
+}
