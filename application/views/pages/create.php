98cbb64065ac809f673ab799bbe1ac2319be2329
Venla Jokikokko <t9jove00@students.oamk.fi>
2020-04-22 19:56:53 +0300

    WIP

---
+++ application/views/pages/create.php
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+<h2>Add a new item</h2>
+=======
+<h2 class="text-muted">Add a new item</h2>
+>>>>>>> b1a0d74019456f2e01f90a898a658af25ca619bf
+<form class="" action="<?php echo site_url('item/insert_item'); ?>" method="post">
+  <label for="title">Item name</label> <br>
+  <input type="text" id="title" name="title" value=""> <br>
+
+  <label for="description">Description</label> <br>
+  <input type="text" id="description" name="description" value=""> <br>
+  <br>
+  <input type="submit" name="" value="Add">
+
+</form>
--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,9 +1,9 @@
 <div class="container">
 <div class="d-flex flex-row bd-highlight mb-3 justify-content-center">
-    <img src="assets/images/photograph-1.jpg" class="p-2 h-180 bd-highlight" alt="Photograph 2" style="height: 240px;">
-    <img src="assets/images/photograph-2.jpg" class="p-2 h-180 bd-highlight" alt="Photograph 2" style="height: 240px;">
-    <img src="assets/images/photograph-3.jpg" class="p-2 h-180 bd-highlight" alt="Photograph 3" style="height: 240px;">
-    <img src="assets/images/photograph-4.jpg" class="p-2 h-180 bd-highlight" alt="Photograph 4" style="height: 240px;">
+    <img src="assets/images/photograph-1.jpg" class="p-2 h-180 bd-highlight" alt="Photograph 2" style="height: 160px;">
+    <img src="assets/images/photograph-2.jpg" class="p-2 h-180 bd-highlight" alt="Photograph 2" style="height: 160px;">
+    <img src="assets/images/photograph-3.jpg" class="p-2 h-180 bd-highlight" alt="Photograph 3" style="height: 160px;">
+    <img src="assets/images/photograph-4.jpg" class="p-2 h-180 bd-highlight" alt="Photograph 4" style="height: 160px;">
 </div>
 <br></br>
 <h2 class="text-muted d-flex justify-content-center">Welcome to Discover Commerce</h2>
